71a2069c15ee5dc044c4e0818186edfd329ab428
Jawwad <jawwadumar99@gmail.com>
2025-01-17 06:10:15 +0530

    Triangle Problem Solved

--- scene.py
+++ scene.py
@@ -1,44 +1,10 @@
 from manim import *
 
-
-class CreateCircle(Scene):
+class LetterByLetter(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
-    
-
-class SquareToCircle(Scene):
-    def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
-
-        square = Square()  # create a square
-        square.rotate(PI / 4)  # rotate a certain amount
-
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
-    
-class SquareAndCircle(Scene):
-    def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-
-        square = Square()  # create a square
-        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
-
-        square.next_to(circle, UP, buff=0.5)  # set the position
-        self.play(Create(circle), Create(square))  # show the shapes on screen    
-
-class AnimatedSquareToCircle(Scene):
-    def construct(self):
-        circle = Circle()  # create a circle
-        square = Square()  # create a square
-
-        self.play(Create(square))  # show the square on screen
-        self.play(square.animate.rotate(PI / 4))  # rotate the square
-        self.play(Transform(square, circle))  # transform the square into a circle
-        self.play(
-            square.animate.set_fill(PINK, opacity=0.5)
-        )  # color the circle on screen
+        text = "Human-like writing"
+        for i, char in enumerate(text):
+            letter = Text(char)
+            self.play(Write(letter), run_time=0.2 + i * 0.05)
+            self.add(letter)  # Keep letters on screen
+        self.wait()
